47b4d7a0229b411f44c896ea1582a8c86e3750da
刘小凡 <601434225@qq.com>
2023-03-06 17:48:06 +0800

    提交代码

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box">你好啊！{{ greet }}</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      greet: '你好',
+    }
+  },
+}
+</script>
+
+<style>
+.box {
+  color: red;
+}
+</style>
