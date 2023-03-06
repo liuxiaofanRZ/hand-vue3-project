55a89d4e6ec2d66538b5e5a8a826ab21f5d61f6f
刘小凡 <601434225@qq.com>
2023-03-06 16:43:19 +0800

    引入vue-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
   mode: 'development',
@@ -8,10 +9,19 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Hello World!',
       template: path.resolve(__dirname, 'index.html'),
     }),
+    new VueLoaderPlugin(),
   ],
 }
